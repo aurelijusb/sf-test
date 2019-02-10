5eaea3304c940c8b453b2f51b6331d1ad4bbda48
Aurelijus Banelis <aurelijus@banelis.lt>
2019-02-10 21:02:55 +0200

    Storing reservation to database

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Slot;
 use App\Form\SlotType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +15,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(Request $request)
+    public function index(Request $request, EntityManagerInterface $manager)
     {
         $slot = new Slot();
         $slot->setDate(new \DateTime());
@@ -21,6 +23,20 @@ class AdminController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $manager->persist($form->getData());
+                $manager->flush();
+                $this->addFlash(
+                    'info',
+                    'Rezervacija atlikta'
+                );
+                return $this->redirectToRoute("admin");
+            } catch (ORMException $exception) {
+                return $this->render('admin/error.html.twig', [
+                    "error" => $exception->getMessage(),
+                ]);
+            }
+
             return $this->render('admin/index.html.twig', [
                 "inputForm" => $form->createView(),
                 "formData" => $form->getData(),
