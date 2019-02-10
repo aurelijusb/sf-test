54391d1e246f39b1ab878b5000353ad7fe6c4cb9
Aurelijus Banelis <aurelijus@banelis.lt>
2019-02-10 20:52:57 +0200

    Form to submit new reservation

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Slot;
+use App\Form\SlotType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -10,8 +13,24 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('admin/index.html.twig', []);
+        $slot = new Slot();
+        $slot->setDate(new \DateTime());
+        $form = $this->createForm(SlotType::class, $slot);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->render('admin/index.html.twig', [
+                "inputForm" => $form->createView(),
+                "formData" => $form->getData(),
+                "state" => "submitted"
+            ]);
+        }
+
+        return $this->render('admin/index.html.twig', [
+            "inputForm" => $form->createView(),
+            "state" => "ready"
+        ]);
     }
 }
---
+++ src/Form/SlotType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Slot;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SlotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Name')
+            ->add('Date')
+            ->add('save', SubmitType::class, ['label' => 'Rezervuoti'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Slot::class,
+        ]);
+    }
+}
